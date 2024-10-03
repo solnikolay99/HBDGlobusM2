7b6d0e3bb5466697dcbc459b7e28bc5e420e3575
solnikolay99 <134811021+solnikolay99@users.noreply.github.com>
2024-10-03 21:25:28 +0300

    Switch from gif to ffmpeg writer. Remove saving pictures from animation procedure

--- Model_2D/show_animation_new.py
+++ Model_2D/show_animation_new.py
@@ -2,18 +2,21 @@
 # @author: L. I. Nurtdinova
 # ******************************************************************************
 
-import datetime
 import glob
 import os
 import shutil
 
+import matplotlib
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
+
 from config import *
 
-
 # plt.rcParams.update({'font.size': 5})
+matplotlib.rcParams['animation.ffmpeg_path'] = "C:\\Program Files\\ffmpeg-2024-09-26-git-f43916e217-essentials_build\\bin\\ffmpeg.exe"
+
 
 # Сортировка по имени названий файлов, хранящих координаты  частиц
 def name_reader(dir_path, pattern):
@@ -33,27 +36,30 @@ def update(frame):
 
     labels = [i for i in range(shape_y)]
     values = np.zeros(shape_y)
+    sum_count_points = 0
     for key in density.keys():
         values[key] = len(density[key])
+        sum_count_points += 1
 
     ax1.clear()
     ax1.set_title(f"{0.25 * frame * pick_every_timeframe: 6.2f} мкс")
     ax1.scatter(m * coords_x, m * coords_y, marker='.', s=0.5, color='#ff531f')
 
     ax2.clear()
-    ax2.set_title("Плотность потока на выходе")
+    ax2.set_title(f"Плотность потока на выходе: {sum_count_points} частиц")
     ax2.barh(labels, values)
 
     plt.xlabel('x, ед.')
     plt.ylabel('y, мм')
     plt.xlim(0, 10)
-    plt.ylim(0, shape_y)
+    plt.ylim(bias - 1, shape_y - bias + 1)
 
     ax1.imshow(mask)
-    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    if saving_to_file:
-        plt.savefig(str(main_dir_path) + '/profs/prof' + str(current_datetime) + '.png', dpi=200, bbox_inches='tight')
-        print(f'{frame} frame was saved')
+    #current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    #if saving_to_file:
+    #    plt.savefig(str(main_dir_path) + '/profs/prof' + str(current_datetime) + '.png', dpi=200, bbox_inches='tight')
+    #    print(f'{frame} frame was saved')
+    print(f'{frame} frame')
 
 
 if __name__ == '__main__':
@@ -62,15 +68,14 @@ if __name__ == '__main__':
     parts_dir_path = main_dir_path + '/parts/'
     out_directory = main_dir_path + '/profs/'
 
-    shutil.rmtree(out_directory, ignore_errors=True)
-    os.makedirs(out_directory, exist_ok=True)
+    #shutil.rmtree(out_directory, ignore_errors=True)
+    #os.makedirs(out_directory, exist_ok=True)
 
     pick_every_timeframe = 5
     pick_every_point = 1
     saving_to_file = 1
     m = 1
 
-    # directory_path = os.getcwd() + '/008'
     mask = np.load(sorted(glob.glob(parts_dir_path + '/' + 'mask*'))[0])
     coords = None
     for file_name in name_reader(parts_dir_path, 'coords*'):
@@ -87,8 +92,9 @@ if __name__ == '__main__':
 
     ani = FuncAnimation(fig, update, frames=range(len(coords)), interval=1)
     if saving_to_file:
-        #ani.save('animation.mp4', writer='ffmpeg')
-        ani.save(main_dir_path + 'animation.gif')
+        FFwriter = animation.FFMpegWriter(fps=10)
+        ani.save(main_dir_path + 'animation.mp4', writer=FFwriter)
+        #ani.save(main_dir_path + 'animation.gif')
     else:
         plt.show()
 
